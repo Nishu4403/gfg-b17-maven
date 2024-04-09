c69825f5fb2fadd543d1dc3e77207f528b626ccf
Nishu4403 <nishathsulikeri@gmail.com>
2024-04-09 18:03:51 +0530

    my first commit

---
+++ src/main/java/mypkg/MyCalc.java
@@ -0,0 +1,25 @@
+package mypkg;
+
+public class MyCalc {
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public int diff(int a, int b)
+	{
+		return a-b;
+	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyCalc calc=new MyCalc();
+		System.out.println("Sum is: "+calc.sum(20,10));
+		System.out.println("Diff is: "+calc.diff(20,10));
+		System.out.println("Diff is: "+calc.mul(20,10));
+	}
+
+}
