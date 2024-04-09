f8ceba86bb4893c5bb24dc3eecc8dea8ddbae2ec
Nishu4403 <nishathsulikeri@gmail.com>
2024-04-09 21:04:09 +0530

    added show function

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -17,6 +17,10 @@ public class MyCalc {
 	{
 		return a/b;
 	}
+	public void show()
+	{
+		System.out.println("Show Function");
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -25,6 +29,7 @@ public class MyCalc {
 		System.out.println("Diff is: "+calc.diff(20,10));
 		System.out.println("Prod is: "+calc.mul(20,10));
 		System.out.println("Div is: "+calc.div(20,10));
+		calc.show();
 	}
 
 }
